62ec9e1757a44f7f87e0add1f517a6908f08094b
infinity <53152093+infinity-zee@users.noreply.github.com>
2020-03-28 20:21:45 +0530

    updated the ipv6

--- Reassign IP.bat
+++ Reassign IP.bat
@@ -1,7 +1,8 @@
 D:
 cd "D:/Abdul Aziz/Dev/Web Development/Website"
 ipconfig > ip
-"update ip"
-git add .
+"address"
+git add *.html *.bat
 git commit -m "updated the ipv6"
 git push -u origin master
+exit
