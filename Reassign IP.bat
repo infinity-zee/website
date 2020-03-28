d8c211088a8da7a79102a353522d4b9a00df9292
infinity <53152093+infinity-zee@users.noreply.github.com>
2020-03-28 20:01:30 +0530

    updated the ipv6

---
+++ Reassign IP.bat
@@ -0,0 +1,6 @@
+D:
+cd "D:/Abdul Aziz/Dev/Web Development/Website"
+"update ip"
+git add .
+git commit -m "updated the ipv6"
+git push -u origin master
---
+++ update ip.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+int main()
+{
+    char line[512];
+    char ip[512];
+    int i, j;
+
+    std::ifstream ipfile("ip");
+
+    while(!ipfile.eof())
+    {
+        ipfile.getline(line, 512, '\n');
+
+        if(strstr(line, "Temporary IPv6 Address. . . . . . :"))
+        {
+            break;
+        }
+    }
+    for (i = 0; line[i] != ':'; i++);
+    i++;
+    i++;
+
+    for (j = 0; line[i] != '\0'; j++, i++)
+    {
+        ip[j] = line[i];
+    }
+    ip[j] = '\0';
+
+    std::cout << ip;
+    ipfile.close();
+
+    std::ofstream htfile("index.html");
+
+    htfile << "<!DOCTYPE html>\n"
+    "<html>\n"
+    "<head>\n"
+    "    <meta http-equiv=\"refresh\" content=\"1; URL=http://[" << ip << "]/\">\n"
+    "</head>\n"
+    "<body>\n"
+    "</body>\n"
+    "</html>\n";
+
+
+    std::cin.get();
+}
